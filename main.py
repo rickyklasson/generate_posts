c50f69e073d102612542294cd4e4ab22cd4104fb
Ricky Klasson <mrklasson@gmail.com>
2022-06-21 20:24:38 +0200

    Refinement

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import datetime
+import importlib
+import os
 import textwrap
 
 from PIL import Image, ImageDraw, ImageFont
@@ -7,17 +10,20 @@ IG_IM_SIZE = (1080, 1350)
 BG_COL = (29, 33, 41)
 WHITE_COL = (255, 255, 255)
 GREY_COL = (182, 182, 182)
+DARK_GREY_COL = (120, 120, 120)
 
 TITLE_FONT_SIZE = 62
 SUB_FONT_SIZE = 49
 POST_FONT_SIZE = 79
 DESC_FONT_SIZE = 31
+FOOTER_FONT_SIZE = 24
 
-TITLE_POS = (120, 80)
+TITLE_POS = (120, 60)
+FOOTER_POS = (720, IG_IM_SIZE[1] - 50)
 SUBTITLE_POS = (TITLE_POS[0], TITLE_POS[1] + TITLE_FONT_SIZE)
 ICON_POS = (810, TITLE_POS[1])
 SUBTITLE_BOTTOM = SUBTITLE_POS[1] + SUB_FONT_SIZE
-POST_TITLE_POS = (SUBTITLE_POS[0], SUBTITLE_POS[1] + SUB_FONT_SIZE + 105)
+POST_TITLE_POS = (SUBTITLE_POS[0], SUBTITLE_POS[1] + SUB_FONT_SIZE + 79)
 DESC_POS = (POST_TITLE_POS[0], POST_TITLE_POS[1] + POST_FONT_SIZE + 50)
 
 N_CHARS_PER_LINE = 45
@@ -27,7 +33,11 @@ FONT_PATH_REG = './fonts/FreeMono.ttf'
 FONT_PATH_BOLD = './fonts/FreeMonoBold.ttf'
 ICON_PATH_PNG = './icons/python_icon.png'
 
+RAW_FOLDER = './raw_material'
+OUTPUT_FOLDER = './finished_posts'
+
 CODE_IMG_MARGIN_RAW = 224  # Whitespace on each side of code window.
+CODE_IMG_WANTED_WIDTH = 896  # A width that fits the instagram aspect.
 
 
 def create_bg():
@@ -39,7 +49,7 @@ def draw_text(img, text, font_size, pos, color, bold=False):
     font_path = FONT_PATH_BOLD if bold else FONT_PATH_REG
     mono_font = ImageFont.truetype(font_path, font_size)
     draw = ImageDraw.Draw(img)
-    draw.text(pos, text, fill=color, font=mono_font)
+    draw.text(pos, text, fill=color, font=mono_font, spacing=10)
 
 
 def draw_image_from_path(img, pos, img_path, width=None):
@@ -52,12 +62,10 @@ def draw_image_from_path(img, pos, img_path, width=None):
 
 
 def resize_code_img(code_img_path):
-    CODE_SEC_WANTED_W = 896  # A width that fits the instagram aspect.
-
     code_img = Image.open(code_img_path)  # Img of code segment.
     code_img_w = code_img.size[0]  # Width of code segment image.
     code_window_w = code_img_w - CODE_IMG_MARGIN_RAW * 2  # Width of code window part of image.
-    scaling = CODE_SEC_WANTED_W / code_window_w  # Scaling factor to get a good size for 1080px wide window.
+    scaling = CODE_IMG_WANTED_WIDTH / code_window_w  # Scaling factor to get a good size for 1080px wide window.
     new_size = tuple(int(scaling * dim) for dim in code_img.size)
 
     return code_img.resize(new_size)
@@ -70,8 +78,12 @@ def calculate_code_img_pos(code_img: Image):
     y_pos = IG_IM_SIZE[1] - code_img.size[1]
     return x_pos, y_pos
 
+def compose_image():
+    today = datetime.datetime.today()
+    today_str = f'{today:%Y%m%d}'
 
-def main():
+    texts = importlib.import_module(f'raw_material.{today_str}.texts')
+
     # Create background.
     img = create_bg()
 
@@ -79,29 +91,48 @@ def main():
     draw_text(img, "Python", TITLE_FONT_SIZE, TITLE_POS, WHITE_COL)
 
     # Draw subtitle.
-    draw_text(img, f"Beginner tip #1", SUB_FONT_SIZE, SUBTITLE_POS, GREY_COL)
+    draw_text(img, texts.subtitle, SUB_FONT_SIZE, SUBTITLE_POS, GREY_COL)
 
     # Draw icon.
     draw_image_from_path(img, ICON_POS, ICON_PATH_PNG, width=TITLE_FONT_SIZE + SUB_FONT_SIZE)
 
     # Draw code image.
-    code_img = resize_code_img('./carbon.png')
+    code_img = resize_code_img(f'raw_material/{today_str}/carbon.png')
     code_img_pos = calculate_code_img_pos(code_img)
 
     img.paste(code_img, code_img_pos, code_img)
 
     # Draw post title
-    draw_text(img, "Comprehensions", POST_FONT_SIZE, POST_TITLE_POS, WHITE_COL, bold=True)
+    draw_text(img, texts.post_title, POST_FONT_SIZE, POST_TITLE_POS, WHITE_COL)
 
     # Draw description.
-    desc = "Similar to list comprehension, Python also supports dictionary comprehension."
-    desc = textwrap.fill(desc, N_CHARS_PER_LINE)
+    desc = textwrap.fill(texts.post_description, N_CHARS_PER_LINE)
     draw_text(img, desc, DESC_FONT_SIZE, DESC_POS, WHITE_COL)
 
     # Draw footer text.
+    draw_text(img, f'@software_bonanza', FOOTER_FONT_SIZE, FOOTER_POS, DARK_GREY_COL)
 
-    # Save output image.
-    img.save(OUTFILE)
+    return img
+
+def generate_out_path():
+    today = datetime.datetime.today()
+    today_str = f'{today:%Y%m%d}'
+    return os.path.join(OUTPUT_FOLDER, today_str, f'post.png')
+
+def save_img_to_file(img: Image, file: str):
+    img.save(file)
+
+
+def main():
+    # Create image path
+    out_path = generate_out_path()
+    os.makedirs(out_path.rsplit('/', 1)[0], exist_ok=True)  # Make all dirs except leaf.
+
+    # Compose image
+    img = compose_image()
+
+    # Save image to file
+    save_img_to_file(img, out_path)
 
 
 if __name__ == '__main__':
