661af26922422bdf0b17960a591291b35194cf9d
Ricky Klasson <mrklasson@gmail.com>
2022-06-25 21:05:23 +0200

    Removed raw material

--- raw_material/20220623/texts.py
+++
@@ -1,3 +0,0 @@
-subtitle = "Beginner tip #1"
-post_title = "Comprehensions"
-post_description = "Similar to list comprehensions, Python also supports dictionary, set and, the slightly more advanced, generator comprehension."
--- raw_material/20220624/texts.py
+++
@@ -1,5 +0,0 @@
-subtitle = "Beginner tip #2"
-post_title = "Enumeration"
-post_description = "The built-in 'enumerate' \
-function makes the item index available when iterating over an iterable \
-(for example a list)."
